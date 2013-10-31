2715616022310607fce3c24ecafce8afb5ded35f
Jacques Stadler <jacques.stadler@gmail.com>
2013-10-31 12:01:41 +0100

    Try out with ruby controller...

--- src/main/java/org/sonar/plugins/ldap/LdapAuthenticationFilter.java
+++ src/main/java/org/sonar/plugins/ldap/LdapAuthenticationFilter.java
@@ -26,15 +26,28 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sonar.api.config.Settings;
+import org.sonar.api.security.UserDetails;
 import org.sonar.api.web.ServletFilter;
 
 
 public class LdapAuthenticationFilter extends ServletFilter {
   
   private static final Logger LOGGER = LoggerFactory.getLogger(LdapAuthenticationFilter.class);
+
+  static final String USER_ATTRIBUTE = "preauth_user";
+  private final static String DEFAULT_PRE_AUTH_HEADER_NAME = "REMOTE_USER";
+  private final Settings settings;
+  
+  public LdapAuthenticationFilter(Settings settings) {
+    this.settings = settings;
+  }
 
   public void init(FilterConfig filterConfig) throws ServletException {
   }
@@ -44,13 +57,32 @@ public class LdapAuthenticationFilter extends ServletFilter {
     return UrlPattern.create("/sessions/new");
   }
 
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
       throws IOException, ServletException {
     
-    LOGGER.debug("Triggered LdapAuthenticationFilter... what now?");
-    chain.doFilter(request, response);
+    LOGGER.debug("Triggered LdapAuthenticationFilter...");
+    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+    HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+    
+    
+    String preAuthHeaderName = StringUtils.defaultString(settings.getString("ldap.preAuthHeaderName"), DEFAULT_PRE_AUTH_HEADER_NAME);
+    
+    
+    String userName = httpServletRequest.getHeader(preAuthHeaderName);
+    if (userName == null) {
+      LOGGER.debug("HTTP Header " + preAuthHeaderName + " not found.");
+      httpServletResponse.sendRedirect("/ldap/unauthorized");
+    } else {
+      LOGGER.debug("User " + userName+ " was preauthenticated.");
+      UserDetails user = new UserDetails();
+      user.setName(userName);
+      request.setAttribute(USER_ATTRIBUTE, user);
+      httpServletResponse.sendRedirect("/ldap/validate");
+    }
   }
-
+  
+  
+  
   public void destroy() {
   }
 
---
+++ src/main/resources/org/sonar/ror/ldap/app/controllers/ldap_controller.rb
@@ -0,0 +1,22 @@
+class LdapController < ApplicationController
+
+  skip_before_filter :check_authentication
+
+  def validate
+    begin
+      self.current_user = User.authenticate(servlet_request['COLOS_USER'], nil, servlet_request)
+
+    rescue Exception => e
+      self.current_user = nil
+      Rails.logger.error(e.message)
+    end
+    redirect_back_or_default(home_url)
+  end
+
+  def unauthorized
+    # this page should be moved to sonar core
+    params[:layout]='false'
+    render :action => 'unauthorized'
+  end
+
+end
