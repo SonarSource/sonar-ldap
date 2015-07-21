c87039d084ffc49017fc81b392c38c85f8191693
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-07-21 12:22:09 +0200

    Upgrade min SQ version from 3.0 to 4.5.1 LTS

--- travis.sh
+++ travis.sh
@@ -29,10 +29,10 @@ IT-LTS)
 
   mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
 
-  travis_download_sonarqube_release "4.5.2"
+  travis_download_sonarqube_release "4.5.1"
 
   cd it
-  mvn -DldapVersion="DEV" -Dsonar.runtimeVersion="4.5.2" -Dmaven.test.redirectTestOutputToFile=false install
+  mvn -DldapVersion="DEV" -Dsonar.runtimeVersion="4.5.1" -Dmaven.test.redirectTestOutputToFile=false install
   ;;
 
 esac
