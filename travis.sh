a43761714e24caa2cf076649f1142bd0138cc3ff
David Gageot <david@gageot.net>
2015-07-22 16:45:07 +0200

    Upgrade travis-utils

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 function installTravisTools {
-  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v12/install.sh | bash
+  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v13/install.sh | bash
   source /tmp/travis-utils/env.sh
 }
 
@@ -15,14 +15,11 @@ CI)
 
 IT-DEV)
   installTravisTools
+  travis_start_xvfb
 
   mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
 
   travis_build_green "SonarSource/sonarqube" "master"
-
-  # required for Selenium
-  export DISPLAY=:99.0
-  travis_start_xvfb
 
   cd it
   mvn -DldapVersion="DEV" -Dsonar.runtimeVersion="DEV" -Dmaven.test.redirectTestOutputToFile=false install
@@ -30,14 +27,11 @@ IT-DEV)
 
 IT-LTS)
   installTravisTools
+  travis_start_xvfb
 
   mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
 
   travis_download_sonarqube_release "4.5.1"
-
-  # required for Selenium
-  export DISPLAY=:99.0
-  travis_start_xvfb
 
   cd it
   mvn -DldapVersion="DEV" -Dsonar.runtimeVersion="4.5.1" -Dmaven.test.redirectTestOutputToFile=false install
