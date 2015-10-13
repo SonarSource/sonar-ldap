b3df5a869639cca5b41f1168fb7921c2c587c86d
Andre Mainka <Andre.Mainka@gmx.net>
2015-10-13 09:50:45 +0200

    LdapContextFactory: When using GSSAPI also initial Context should be established using GSSAPI checkPasswordUsingGssapi: when checking password, reset Configuration

--- src/main/java/org/sonar/plugins/ldap/LdapAuthenticator.java
+++ src/main/java/org/sonar/plugins/ldap/LdapAuthenticator.java
@@ -96,6 +96,7 @@ public class LdapAuthenticator implements LoginPasswordAuthenticator {
     InitialDirContext context = null;
     try {
       context = contextFactories.get(ldapKey).createUserContext(principal, password);
+	  LOG.debug("Password valid for user {} in server {}!", new Object[] {principal, ldapKey});
       return true;
     } catch (NamingException e) {
       LOG.debug("Password not valid for user {} in server {}: {}", new Object[] {principal, ldapKey, e.getMessage()});
@@ -106,24 +107,31 @@ public class LdapAuthenticator implements LoginPasswordAuthenticator {
   }
 
   private boolean checkPasswordUsingGssapi(String principal, String password, String ldapKey) {
-    // Use our custom configuration to avoid reliance on external config
-    Configuration.setConfiguration(new Krb5LoginConfiguration());
-    LoginContext lc;
-    try {
-      lc = new LoginContext(getClass().getName(), new CallbackHandlerImpl(principal, password));
-      lc.login();
-    } catch (LoginException e) {
-      // Bad username: Client not found in Kerberos database
-      // Bad password: Integrity check on decrypted field failed
-      LOG.debug("Password not valid for {} in server {}: {}", new Object[] {principal, ldapKey, e.getMessage()});
-      return false;
-    }
-    try {
-      lc.logout();
-    } catch (LoginException e) {
-      LOG.warn("Logout fails", e);
-    }
+    
+	Configuration currentConfiguration = Configuration.getConfiguration();
+	try {
+	    // Use our custom configuration to avoid reliance on external config
+	    Configuration.setConfiguration(new Krb5LoginConfiguration());
+	    LoginContext lc;
+	    try {
+	      lc = new LoginContext(getClass().getName(), new CallbackHandlerImpl(principal, password));
+	      lc.login();
+	    } catch (LoginException e) {
+	      // Bad username: Client not found in Kerberos database
+	      // Bad password: Integrity check on decrypted field failed
+	      LOG.debug("Password not valid for {} in server {}: {}", new Object[] {principal, ldapKey, e.getMessage()});
+	      return false;
+	    }
+	    try {
+	      lc.logout();
+	    } catch (LoginException e) {
+	      LOG.warn("Logout fails", e);
+	    }
+		LOG.debug("Password valid for user {} in server {}!", new Object[] {principal, ldapKey});
     return true;
+	} finally {
+		Configuration.setConfiguration(currentConfiguration);
+	}
   }
 
 }
