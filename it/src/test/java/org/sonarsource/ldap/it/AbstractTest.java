c8c438e7b602ce01f50b164a3fbcd116406b6815
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-06-09 10:15:25 +0200

    Revert "Filter out javadoc jar in IT when searching for the plugin jar"
    This reverts commit 7d81538d2f5434baba995bba6249f92469b09d40.

--- it/src/test/java/org/sonarsource/ldap/it/AbstractTest.java
+++ it/src/test/java/org/sonarsource/ldap/it/AbstractTest.java
@@ -19,53 +19,14 @@
  */
 package org.sonarsource.ldap.it;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.Iterables;
 import com.sonar.orchestrator.locator.FileLocation;
 import com.sonar.orchestrator.locator.Location;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOCase;
-import org.apache.commons.io.filefilter.IOFileFilter;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
-
-import static java.lang.String.format;
-import static org.apache.commons.io.filefilter.FileFilterUtils.and;
-import static org.apache.commons.io.filefilter.FileFilterUtils.notFileFilter;
 
 public abstract class AbstractTest {
 
   public static Location ldapPluginLocation() {
-    return byWildcardMavenFilename(new File("../sonar-ldap-plugin/target/"), "sonar-ldap-plugin-*.jar");
-  }
-
-  /**
-   * This method must be remove when the byWildcardMavenFilename method provided by Orchestrator will filter out javadoc jar
-   */
-  private static FileLocation byWildcardMavenFilename(File directory, String wildcardFilename) {
-    if (!directory.exists()) {
-      throw new IllegalStateException(format("Directory [%s] does not exist", directory));
-    }
-    IOFileFilter artifactFilter = new WildcardFileFilter(wildcardFilename, IOCase.SENSITIVE);
-    IOFileFilter sourcesFilter = notFileFilter(new WildcardFileFilter("*-sources.jar"));
-    IOFileFilter testsFilter = notFileFilter(new WildcardFileFilter("*-tests.jar"));
-    IOFileFilter javadocFilter = notFileFilter(new WildcardFileFilter("*-javadoc.jar"));
-    IOFileFilter filters = and(artifactFilter, sourcesFilter, testsFilter, javadocFilter);
-    Collection<File> files = new ArrayList<>(FileUtils.listFiles(directory, filters, null));
-    return getOnlyFile(directory, wildcardFilename, files);
-  }
-
-
-  private static FileLocation getOnlyFile(File directory, String wildcardFilename, Collection<File> files) {
-    if (files.isEmpty()) {
-      throw new IllegalStateException(format("No files match [%s] in directory [%s]", wildcardFilename, directory));
-    }
-    if (files.size() > 1) {
-      throw new IllegalStateException(format("Multiple files match [%s] in directory [%s]: %s", wildcardFilename, directory, Joiner.on(", ").join(files)));
-    }
-    return FileLocation.of(Iterables.getOnlyElement(files));
+    return FileLocation.byWildcardMavenFilename(new File("../sonar-ldap-plugin/target/"), "sonar-ldap-plugin-*.jar");
   }
 
 }
